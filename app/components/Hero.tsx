9d25cf09362c363e879c51d5019d3ae5aed24aff
phyothantzin <phyothantzin.w@gmail.com>
2023-12-31 18:54:33 +0630

    initial startup

---
+++ app/components/Hero.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+      <section className="relative w-full h-screen mx-auto">Hero</section>;
+    </div>
+  );
+};
+
+export default Hero;
